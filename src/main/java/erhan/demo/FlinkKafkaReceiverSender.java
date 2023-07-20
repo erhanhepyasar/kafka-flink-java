276eff748bcd02deaeee147d4e96d49156217297
erhanhepyasar <erhan.hepyasar@gmail.com>
2023-07-20 20:31:22 +0300

    libraries updated

--- src/main/java/erhan/demo/FlinkKafkaReceiverSender.java
+++ src/main/java/erhan/demo/FlinkKafkaReceiverSender.java
@@ -1,22 +1,25 @@
 package erhan.demo;
 
-import java.util.Properties;
-
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+
 
 public class FlinkKafkaReceiverSender {
-	
-	final static String SERVER = "localhost:9092";
-	final static String INPUT_TOPIC = "input-topic";
-	final static String OUTPUT_TOPIC = "output-topic";
+
+	public static final String SERVER = "localhost:9092";
+	public static final String INPUT_TOPIC = "input-topic";
+	public static final String OUTPUT_TOPIC = "output-topic";
+	public static final String FLINK_CONSUMER_GROUP = "flink-consumer-group";
 
 	public static void main(String[] args) throws Exception {
-		 StreamStringOperation(SERVER, INPUT_TOPIC, OUTPUT_TOPIC);
+		 StreamStringOperation();
 	}
 	
 	public static class StringCapitalize implements MapFunction<String, String> {
@@ -25,28 +28,39 @@ public class FlinkKafkaReceiverSender {
 	        return data.toUpperCase();
 	    }
 	}
-	
-	public static void StreamStringOperation(String server, String inputTopic, String outputTopic ) throws Exception {
-	    final StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
-	    final FlinkKafkaConsumer011<String> flinkKafkaConsumer = createStringConsumerForTopic(inputTopic, server);
-	    final FlinkKafkaProducer011<String> flinkKafkaProducer = createStringProducer(outputTopic, server);
-	    final DataStream<String> stringInputStream = environment.addSource(flinkKafkaConsumer);
-	  
-	    stringInputStream.map(new StringCapitalize()).addSink(flinkKafkaProducer);
-	    environment.execute();
-	}
-	
-	public static FlinkKafkaConsumer011<String> createStringConsumerForTopic(String topic, String kafkaAddress) {
-		final Properties props = new Properties();
-		props.setProperty("bootstrap.servers", kafkaAddress);
 
-		return new FlinkKafkaConsumer011<>(topic, new SimpleStringSchema(), props);
-	}
-	
-	public static FlinkKafkaProducer011<String> createStringProducer(String topic, String kafkaAddress){
+	public static void StreamStringOperation() throws Exception {
+		final StreamExecutionEnvironment environment = StreamExecutionEnvironment.getExecutionEnvironment();
+		final KafkaSource<String> kafkaSource = createKafkaSource();
+		final KafkaSink<String> kafkaSink = createKafkaSink();
 
-		return new FlinkKafkaProducer011<>(kafkaAddress, topic, new SimpleStringSchema());
+		// Consuming from Kafka
+		DataStreamSource<String> stringInputStream = environment.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source");
+		stringInputStream.map(new StringCapitalize()).sinkTo(kafkaSink);
+		environment.execute("Read from Kafka");
+
 	}
 
+	public static KafkaSource<String> createKafkaSource() {
+		return KafkaSource.<String>builder()
+				.setBootstrapServers(SERVER)
+				.setTopics(INPUT_TOPIC)
+				.setGroupId(FLINK_CONSUMER_GROUP)
+				.setStartingOffsets(OffsetsInitializer.latest())
+				.setValueOnlyDeserializer(new SimpleStringSchema())
+				.build();
+	}
+
+	public static KafkaSink<String> createKafkaSink(){
+		KafkaRecordSerializationSchema<String> serializer = KafkaRecordSerializationSchema.builder()
+				.setValueSerializationSchema(new SimpleStringSchema())
+				.setTopic(OUTPUT_TOPIC)
+				.build();
+
+		return KafkaSink.<String>builder()
+				.setBootstrapServers(SERVER)
+ 				.setRecordSerializer(serializer)
+				.build();
+	}
 
 }
